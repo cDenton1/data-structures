7988b5dd3d2932cf3888130a44c4192770232e45
Denton <163344926+cDenton1@users.noreply.github.com>
2026-02-12 19:07:01 -0700

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,9 +1,10 @@
 // SelectionSort uses O(n^2). This means the more data, the more time it'll take to process the data.
+import java.util.Comparator;
 
 public class SelectionSort {
 
     // This public static method is T in order to account for other data types.
-    public static <T extends Comparable<T>> void selectionSort(T[] array) {
+    public static <T> void selectionSort(T[] array, Comparator<T> comparator) {
         
         // Begins with 0 as a starting point to work off of all the upcoming numbers.
         for(int i = 0; i < array.length - 1; i++) { // First index of the unsorted section.
@@ -11,7 +12,7 @@ public class SelectionSort {
              
             // The following inner loop is to scan the rest of the array and see if there's anything smaller than the current minimum.
             for(int j = i + 1; j < array.length; j++) {
-                if (array[j].compareTo(array[min]) < 0) {
+                if (comparator.compare(array[j], array[min]) < 0) {
                     min = j;
                 }
             }
