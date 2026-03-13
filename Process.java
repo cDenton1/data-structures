fb3b3d4f89898d40429339eb4d67f69a2b3ea159
iafework <166059146+iafework@users.noreply.github.com>
2026-03-12 21:04:00 -0600

    Update Process.java

--- Process.java
+++ Process.java
@@ -1,4 +1,5 @@
-public class Process {
+
+public class Process implements Comparable<Process> {
 
     // Attributes
     private int processID;
@@ -41,7 +42,17 @@ public class Process {
     // Summary of the process
     public String toString() {
         return "Process ID: " + processID + " | Arrival Time: " + arrivalTime + " | Burst Time: " + burstTime;
-
     }
 
+    // compareTo() to compare the arrival times
+    public int compareTo(Process other) {
+        int comparison = Integer.compare(this.arrivalTime, other.arrivalTime);
+        if (comparison != 0) { // If the arrival time isn't the same
+            return comparison;
+        }
+        else { 
+            return Integer.compare(this.processID, other.processID); // If the arrival times match, compare the process IDs instead.
+        }
+        
+    }
 }
